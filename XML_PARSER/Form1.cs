8c75b2c0bab0a2fea2c47f4c2d10cf33aa67d4c4
interstellarspace <eggy_lumbres@yahoo.com>
2015-11-25 23:53:29 -0600

    11-25 cut

--- XML_PARSER/Form1.cs
+++ XML_PARSER/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+using XmlDocument;
 
 namespace XML_PARSER
 {
@@ -15,6 +18,12 @@ namespace XML_PARSER
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlDocument.XmlEdmx xDoc;
+            xDoc = new XmlDocument.XmlEdmx("EDMX.edmx", "edmx");
         }
     }
 }
---
+++ XmlDocument/XmlEdmx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XmlDocument
+{
+    public class XmlEdmx : XmlDocument
+    {
+        public IEnumerable<XElement> ieStorageModels;
+        public IEnumerable<XElement> ieConceptualModels;
+        public IEnumerable<XElement> ieMappings;
+
+        public XmlEdmx(String strFilename) : base(strFilename) 
+        { 
+            InitRuntime();
+        }
+
+        public XmlEdmx(String strFilename, String strNameSpace) : base(strFilename, strNameSpace) 
+        { 
+            InitRuntime();
+        }
+
+        private void InitRuntime()
+        {
+            ieStorageModels = InitModels("StorageModels");
+            ieConceptualModels = InitModels("ConceptualModels");
+            ieMappings = InitModels("Mappings");
+        }
+
+        private IEnumerable<XElement> InitModels(String model)
+        {
+            var result = GetNodes(model).Descendants();
+            return result;
+        }
+
+
+    }
+}
